0ffc9028fa6e91a1c4c66c1fe75e72bf1b24979a
clivehot <36730246+clivehot@users.noreply.github.com>
2018-02-24 20:10:30 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -2,25 +2,32 @@
 
 /* It's better to define the empty variables outside so that HTML has access to them */
 $name = "";
-$required = "";
+$required = "";$emailErr = "";
 $sent = "";
 
 if( isset($_POST['submit'])) {
-	$name = $_POST['name'];    
-	if(!preg_match("/^[a-zA-Z0-9 ]*$/",$name)) {
-	    $required = "*Only letters and numbers allowed";
-	} else {
-	    $sent = "Your message has been posted"; 
-	}
-
-function security($post) {
+     function security($post) {
 		$x = trim($post); 
 		$x = stripslashes($x);
 		$x = htmlspecialchars($x);  
 		return $x;
 	}
 
+	/* 	If form is submitted $name becomes the name that is submitted */
+	$name = $_POST['name'];
+    if(!preg_match("/^[a-zA-Z0-9 ]*$/",$name)) {
+	    $required = "*Only letters and numbers allowed";
+	} 
+    
+    $email =$_POST['email'];
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailErr = "Invalid email format"; 
+    }
+
+    /* Inserts $name into the security function prevents hacking*/
     $name = security($name);
+    $email = security($email); 
+    echo $name;
     
 }
 
@@ -39,8 +46,10 @@ function security($post) {
 
 <form action="" method="POST" >
     Name: <input type="text" name="name" required><span id="error" ><?php echo $required; ?></span>
+    Email: <input type="text" name="email" required><span id="error" ><?php echo $emailErr; ?></span>
     <input type="submit" name="submit" value="Submit" >
 </form>
 <p><?php echo $sent ?></p>
+
 </body>
 </html>
