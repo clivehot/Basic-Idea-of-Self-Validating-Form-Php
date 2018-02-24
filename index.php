f9b942c7591cc4de54b70eea6eb12ec5d196df84
clivehot <36730246+clivehot@users.noreply.github.com>
2018-02-24 20:12:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,28 +11,27 @@ if( isset($_POST['submit'])) {
 		$x = stripslashes($x);
 		$x = htmlspecialchars($x);  
 		return $x;
-	}
+     }
 
-	/* 	If form is submitted $name becomes the name that is submitted */
-	$name = $_POST['name'];
-    if(!preg_match("/^[a-zA-Z0-9 ]*$/",$name)) {
+     /* If form is submitted $name becomes the name that is submitted */
+     $name = $_POST['name'];
+     if(!preg_match("/^[a-zA-Z0-9 ]*$/",$name)) {
 	    $required = "*Only letters and numbers allowed";
-	} 
+     } 
     
-    $email =$_POST['email'];
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $emailErr = "Invalid email format"; 
-    }
+     $email =$_POST['email'];
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+     $emailErr = "Invalid email format"; 
+     }
 
-    /* Inserts $name into the security function prevents hacking*/
-    $name = security($name);
-    $email = security($email); 
-    echo $name;
+     /* Inserts $name into the security function prevents hacking*/
+     $name = security($name);
+     $email = security($email); 
+     echo $name;
     
 }
 
 ?>
-
 
 <html>
 <head>
